c06dec6cf3acdf215067f16766c674dfe73a597c
Chris Price <price.c@gmail.com>
2012-08-07 14:48:35 +0100

    allow changing the frame count without clearing the cube

--- client/image-cube.js
+++ client/image-cube.js
@@ -5,13 +5,14 @@ define([ './jquery', './transform' ], function($, css3) {
 	}
 
 	Cube.prototype.add = function(image) {
+		var children = this.container.children();
 		var ctx;
-		for ( var i = this.children.length - 1; i > 0; i--) {
-			ctx = this.children[i].getContext('2d');
+		for ( var i = children.length - 1; i > 0; i--) {
+			ctx = children[i].getContext('2d');
 			ctx.clearRect(0, 0, this.width, this.height);
-			ctx.drawImage(this.children[i - 1], 0, 0);
+			ctx.drawImage(children[i - 1], 0, 0);
 		}
-		ctx = this.children[0].getContext('2d');
+		ctx = children[0].getContext('2d');
 		ctx.clearRect(0, 0, this.width, this.height);
 		ctx.drawImage(image, 0, 0);
 	};
@@ -19,24 +20,38 @@ define([ './jquery', './transform' ], function($, css3) {
 	Cube.prototype.setDimensions = function(width, height, depth, count) {
 		this.width = width;
 		this.height = height;
+		this.depth = depth;
+		this.setCount(count);
+	};
 
-		this.container.empty();
-		this.children = [];
+	Cube.prototype.setCount = function(count) {
+		var delta = -this.depth / count;
+		var z = this.depth / 2;
+		if (count % 2 === 0) {
+			z += delta / 2;
+		}
+		console.log(delta, z);
+		var children = this.container.children();
 
-		var delta = Math.round(depth / count);
-		for ( var z = Math.round(depth / 2); z >= -Math.round(depth / 2); z -= delta) {
-			var canvas = $('<canvas/>').width(this.width).height(this.height).attr({
-				width : this.width,
-				height : this.height
-			}).css({
-				position : 'absolute',
-				top : '50%',
-				left : '50%',
-				marginTop : -this.height / 2,
-				marginLeft : -this.width / 2
-			}).translate(0, 0, z);
-			this.container.append(canvas);
-			this.children.push(canvas[0]);
+		for ( var i = 0; i < count; i++) {
+			var canvas = children.eq(i);
+			if (!canvas.length) {
+				canvas = $('<canvas/>').width(this.width).height(this.height).attr({
+					width : this.width,
+					height : this.height
+				}).css({
+					position : 'absolute',
+					top : '50%',
+					left : '50%',
+					marginTop : -this.height / 2,
+					marginLeft : -this.width / 2
+				}).appendTo(this.container);
+			}
+			canvas.clearTransform().translate(0, 0, z);
+			z += delta;
+		}
+		for (i = count; i < children.length; i++) {
+			children.eq(i).remove();
 		}
 	};
 
