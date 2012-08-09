81d35c8aa8a5c0af7daad656f1f3789981999cd7
Chris Price <price.c@gmail.com>
2012-08-09 16:36:46 +0100

    ooooh pink lines\!

--- js/particles.js
+++ js/particles.js
@@ -18,7 +18,8 @@ require([ './webcam', './jquery', './mouse-control', './transform', './Three' ],
 	var scene = new THREE.Scene();
 
 	// create the particle variables
-	var particleDepth = 256, particleCount = 16, particles = new THREE.Geometry();
+	var depth = 256, width = 256, segmentsDepth = 16, segmentsWidth = 16;
+	var plane = new THREE.PlaneGeometry(width, depth, segmentsWidth, segmentsDepth);
 	// var pMaterial = new THREE.ParticleBasicMaterial({
 	// color : 0xFFFFFF,
 	// size : 1
@@ -38,38 +39,24 @@ require([ './webcam', './jquery', './mouse-control', './transform', './Three' ],
 			value : []
 		}
 	};
-	console.log(vShader.text(), fShader.text());
 	var shaderMaterial = new THREE.ShaderMaterial({
 		attributes : attributes,
 		vertexShader : vShader.text(),
 		fragmentShader : fShader.text()
 	});
-
-	var delta = particleDepth / particleCount;
-	var offset = particleDepth / 2;
-	// now create the individual particles on 3d grid pattern
-	for ( var z = 0; z < particleCount; z++) {
-		for ( var y = 0; y < particleCount; y++) {
-			for ( var x = 0; x < particleCount; x++) {
-				var particle = new THREE.Vector3(x * delta - offset, y * delta - offset, z * delta
-						- offset);
-				// add it to the geometry
-				particles.vertices.push(particle);
-				attributes.aColor.value.push(new THREE.Vector4(x / particleCount,
-						y / particleCount, z / particleCount, 0.5));
-			}
-		}
+	var sphereMaterial = new THREE.MeshLambertMaterial({
+		color : 0xCC0000
+	});
+	var radius = 50, segments = 16, rings = 16; // 16 segments means 17 vertices
+	var sphere = new THREE.SphereGeometry(radius, segments, rings);
+	var mesh = new THREE.Mesh(plane, shaderMaterial);
+	console.log(mesh.geometry.vertices.length);
+	for ( var i = 0; i < mesh.geometry.vertices.length; i++) {
+		attributes.aColor.value.push(new THREE.Vector4(i % 2 == 1, i % 2 == 0, 0.5, i % 2 == 1));
 	}
 
-	// create the particle system
-	var particleSystem = new THREE.ParticleSystem(particles, shaderMaterial);
-
-	// sort the particle system so that z order is taken into account when
-	// drawing
-	particleSystem.sortParticles = true;
-
 	// add it to the scene
-	scene.add(particleSystem);
+	scene.add(mesh);
 
 	// create a point light
 	var pointLight = new THREE.PointLight(0xFFFFFF);
@@ -98,8 +85,7 @@ require([ './webcam', './jquery', './mouse-control', './transform', './Three' ],
 	$container.append(renderer.domElement);
 
 	new MouseControl($container).apply = function() {
-		particleSystem.rotation
-				.set(this.rotX / 360 * 2 * Math.PI, this.rotY / 360 * 2 * Math.PI, 0);
+		mesh.rotation.set(this.rotX / 360 * 2 * Math.PI, this.rotY / 360 * 2 * Math.PI, 0);
 	};
 
 	function loop() {
